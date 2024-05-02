83697db315d92db87aa89d4ab395e05b78494e23
hansraptor <hannes.swanepoel@disraptor.co.za>
2024-05-02 11:34:13 +0200

    fix local variable reference

--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@ locals {
 }
 
 resource "aws_s3_bucket" "s3b-tf-cloud-test" {
-    bucket = "hansraptor-terraform-cloud-test-attempt${attempt}"
+    bucket = "hansraptor-terraform-cloud-test-attempt${local.attempt}"
 
     tags = {
         Name = "S3 Bucket - Terraform Cloud Provisioned - ${var.tag-task}"
-        Task = "${var.tag-task} - Attempt ${attempt}"
+        Task = "${var.tag-task} - Attempt ${local.attempt}"
         Creator = var.aws_credentials_profile
     }
 }
