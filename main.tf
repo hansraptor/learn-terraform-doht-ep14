c2c1c632e7c6b592fcc9a16f6b9cb883fa87c97c
hansraptor <hannes.swanepoel@disraptor.co.za>
2024-05-02 10:16:40 +0200

    i guess tf cloud only triggers on pull request mergers - update bucket name - update task tag

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 resource "aws_s3_bucket" "s3b-tf-cloud-test" {
-    bucket = "hansraptor-terraform-cloud-test-attempt2"
+    bucket = "hansraptor-terraform-cloud-test-attempt02"
 
     tags = {
         Name = "S3 Bucket - Terraform Cloud Provisioned - ${var.tag-task}"
